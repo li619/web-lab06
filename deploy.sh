9e2c4bc75b206a36887b6b4ffea72bc9c8476690
Morry <3360517810@qq.com>
2024-12-04 21:01:19 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,33 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+echo "开始部署..."
+
+# 删除旧的 dist 目录
+rm -rf dist
+
 # 构建
 npm run build
 
 # 进入构建输出目录
 cd dist
 
+# 初始化 git 仓库
 git init
+
+# 配置 Git（使用你的 GitHub 用户名和邮箱）
+git config user.name "li619"
+git config user.email "你的GitHub邮箱"
+
+# 添加所有文件
 git add -A
-git commit -m 'deploy'
 
-# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:你的用户名/web-lab06.git master:gh-pages
+# 提交更改
+git commit -m "deploy to gh-pages"
 
-cd - 
+# 推送到 gh-pages 分支
+git push -f https://github.com/li619/web-lab06.git master:gh-pages
+
+cd -
+
+echo "部署完成!"
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/web-lab06/'  // 替换成你的仓库名
+    ? '/web-lab06/'
     : '/',
   outputDir: 'dist'
 })
